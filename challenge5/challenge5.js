41816e1c298aa521fab2ca7f4fb422f1ea3c3ecc
bachiv <137339684+bachiv@users.noreply.github.com>
2023-06-21 18:31:21 -0300

    Add files via upload

---
+++ challenge5/challenge5.js
@@ -0,0 +1,20 @@
+let numero1 = 11;
+let numero2 = 12;
+let resultado;
+
+console.log("la suma de 11+12 es:", numero1 + numero2);
+console.log( "la resta de 11-12 es:", numero1 - numero2);
+console.log("el producto entre 11*12 es:", numero1 * numero2);
+console.log("la división entre 11/12 es:", numero1 / numero2);
+
+if(numero1 % 2 === 0){
+    console.log(`el primer numero ${numero1} es par`)
+}else{
+    console.log(`el primer numero ${numero1} es impar`)
+}
+
+if(numero2 % 2 === 0){
+    console.log(`el segundo numero ${numero2} es par`)
+}else{
+    console.log(`el segundo numero ${numero2} es impar`)
+}
